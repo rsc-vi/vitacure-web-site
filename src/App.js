37dca07dc8e1673cd552a5dacd23d3bd6b326af7
Gui <guihenri0708@gmail.com>
2024-12-19 12:11:47 -0300

    desisto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      {/* <h1> vitacure </h1> */}
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/consulta" element={<Consulta />} />
+      </Routes>
+    </>
   );
 }
-
 export default App;
---
+++ src/componentes/consulta.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Consulta = () => (
+  <div className="container mt-5">
+    <h1>Consulta </h1>
+    <p>Review App</p>
+  </div>
+);
+
+export default Consulta;
