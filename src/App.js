7a66744aa79a02651d6ca2927a66f4841d7e5e56
Gui <guihenri0708@gmail.com>
2025-01-22 10:36:37 -0300

    é sobre isso

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Consulta from './componentes/consulta';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/consulta" element={<Consulta />} />
+        <Route path="/" element={<Consulta />} />
+        <Route path="/consulta" element={<Index />} />
       </Routes>
     </>
   );
