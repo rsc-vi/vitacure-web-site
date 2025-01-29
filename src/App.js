e0b0773f0429efe8bc4ba030e8738647bd97b166
Gui <guihenri0708@gmail.com>
2025-01-29 10:43:31 -0300

    alguns ajustes

--- src/App.js
+++ src/App.js
@@ -8,29 +8,9 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Consulta />} />
+        <Route path="/consulta" element={<Consulta />} />
         <Route path="/" element={<Home />} />
       </Routes>
-      const Navbar = () => (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="container">
-            <Link className="navbar-brand" to="/">React Review</Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/">Home</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/consulta">Consulta</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      );
     </>
   );
 }
